ed76a118a765c3495de3d9df7603d1780face462
Caroline Brownlee <caroline.brownlee@carecorenational.com>
2020-08-19 14:42:44 -0500

    added javascript functionality

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -4,3 +4,10 @@ console.log("Plural Sight");
 
 var theForm = document.getElementById("theForm");
 theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    alert("Buying Item");
+});
+
+var productInfo = document.getElementsByClassName("product-props");
